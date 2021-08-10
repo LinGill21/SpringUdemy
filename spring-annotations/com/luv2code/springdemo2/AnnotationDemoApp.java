dc307ce842a0ec5b4c1108ed2210521f6858ee47
Lindsay Gillespie <lind-gillespie@sbcglobal.net>
2021-08-10 12:07:49 -0500

    Explicit Component Name

---
+++ spring-annotations/com/luv2code/springdemo2/AnnotationDemoApp.java
@@ -0,0 +1,21 @@
+package com.luv2code.springdemo2;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AnnotationDemoApp {
+
+	public static void main(String[] args) {
+		//read spring config file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		//get the bean from spring
+		Coach theCoach = context.getBean("thatSillyCoach",Coach.class);
+		//call a method on bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//close the context
+		context.close();
+
+	}
+
+}
---
+++ spring-annotations/com/luv2code/springdemo2/TennisCoach.java
@@ -0,0 +1,14 @@
+package com.luv2code.springdemo2;
+
+import org.springframework.stereotype.Component;
+import com.luv2code.springdemo2.Coach;
+
+@Component("thatSillyCoach")
+public class TennisCoach implements Coach {
+
+	@Override
+	public String getDailyWorkout() {
+		return "Practice your backhand volley";
+	}
+
+}
