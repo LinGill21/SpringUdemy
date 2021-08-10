fcb1432c8b420272f69b3c43b21062c6dc783068
Lindsay Gillespie <lind-gillespie@sbcglobal.net>
2021-08-10 14:55:58 -0500

    Field Injection Annotation

--- spring-annotations/com/luv2code/springdemo2/TennisCoach.java
+++ spring-annotations/com/luv2code/springdemo2/TennisCoach.java
@@ -6,13 +6,23 @@ import com.luv2code.springdemo2.Coach;
 
 @Component
 public class TennisCoach implements Coach {
-
+	@Autowired
 	private FortuneService fortuneService;
+	public TennisCoach() {System.out.println("Tennis coach contructor");}
+	/*
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
 		fortuneService= theFortuneService;
 	}
+	*/
 	
+	//define setter method
+	/*@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println("In tennis fortune setter");
+		this.fortuneService = fortuneService;
+	}
+*/
 	@Override
 	public String getDailyWorkout() {
 		return "Practice your backhand volley";
