380c9a0b2562e66baf3cb04c72354e39d203e403
Lindsay Gillespie <lind-gillespie@sbcglobal.net>
2021-08-11 10:07:54 -0500

    Spring Config no xml

---
+++ spring-annotations/com/luv2code/springdemo2/SportConfig.java
@@ -0,0 +1,10 @@
+package com.luv2code.springdemo2;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.luv2code.springdemo2")
+public class SportConfig {
+	
+}
